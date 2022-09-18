296b4172d2b993ef0bfd41c7e4a94b62cc8656a0
Vedhas <63504153+vedkharche538@users.noreply.github.com>
2022-09-18 18:01:19 +0530

    Different wasy you can comment in bash

---
+++ comments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#This is a single-line comment in Bash Script.
+echo "Enter your name:"
+read $name
+echo
+#echo output, its also a single line comment
+echo "The current user name is $name"
+#This is another single line comment
+<<COMMENTS
+    This is the first comment  
+    This is the second comment  
+    This is the third comment  
+COMMENTS
+
+: '  
+This is the first comment  
+This is the second comment  
+This is the third comment  
+'
+
+echo "Multiline comment"
